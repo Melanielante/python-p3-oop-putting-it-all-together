0a3e91e0546a1d6a5425a797ecc4057e6db5bf5a
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-09-02 15:04:26 +0300

    labs

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # placeholder for setter
+        self.size = size   # triggers setter validation
+        self.condition = None  # optional default
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
     pass
